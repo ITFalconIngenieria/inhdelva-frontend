6cfbae1679ba7d60645da342274507c79dccda43
Lesly Peña <pleslysarahi@gmail.com>
2020-07-24 19:09:04 -0600

    login y diseño de menu

--- inhdelva/src/app/app-routing.module.ts
+++ inhdelva/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Importaciones de componentes
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: WelcomeComponent,
+    children: [
+      { path: 'proveedores', component: ProveedoresComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
