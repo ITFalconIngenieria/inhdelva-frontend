f230cc51f5cc01fa9301260fdd9cc844ea35a12e
Lesly Peña <pleslysarahi@gmail.com>
2020-09-02 00:42:25 -0600

    add cambios inpurt busquedad

--- inhdelva/src/app/servicios/medidores.service.ts
+++ inhdelva/src/app/servicios/medidores.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
+const apiUrl = environment.apiUrl;
 @Injectable({
   providedIn: 'root'
 })
 export class MedidoresService {
 
-constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getMedidores() {
+    return this.http.get(`${apiUrl}`);
+  }
 
 }
