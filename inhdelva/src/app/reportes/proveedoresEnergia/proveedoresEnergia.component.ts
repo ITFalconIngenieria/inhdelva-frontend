5bfccd9c77c324cd38f2e52b34e364ed477cff33
Lesly Peña <pleslysarahi@gmail.com>
2021-01-15 01:45:22 -0600

    add PDF Reportes, MV en un 40%

--- inhdelva/src/app/reportes/proveedoresEnergia/proveedoresEnergia.component.ts
+++ inhdelva/src/app/reportes/proveedoresEnergia/proveedoresEnergia.component.ts
@@ -4,6 +4,8 @@ import { ActoresService } from '../../servicios/actores.service';
 import * as moment from 'moment';
 import swal from 'sweetalert';
 import { NgxSpinnerService } from 'ngx-spinner';
+import 'jspdf-autotable';
+import jspdf from 'jspdf';
 
 @Component({
   selector: 'app-proveedoresEnergia',
@@ -21,6 +23,12 @@ export class ProveedoresEnergiaComponent implements OnInit {
   listaIDProveedores: any[] = [];
   listaTotales: any[] = [];
   dataExport: any[] = [];
+
+  cols: any[];
+  exportColumns: any[];
+  dataPDF: any[] = [];
+  colsExport: any[] = [];
+  dataPDFExport: any[] = [];
 
   constructor(
     private reporteService: ReportesService,
@@ -138,6 +146,7 @@ export class ProveedoresEnergiaComponent implements OnInit {
                 'PROVEEDOR': y.proveedor,
                 'Año': moment(y.fecha).format('YYYY'),
                 'Mes': moment(y.fecha).format('MM'),
+                'Fecha': moment(y.fecha).format('MM/YYYY'),
                 'Consumo Energia activa (kWh/mes)': y.consumoEnergiaActiva,
                 'Energia activa exportada (kWh/mes)': y.energiaActivaExportada,
                 'Demanda potencia maxima (kW)': y.demandaPotenciaMaxima,
@@ -158,7 +167,8 @@ export class ProveedoresEnergiaComponent implements OnInit {
               this.dataExport = [...this.dataExport, {
                 'PROVEEDOR': x.proveedor,
                 'Año': '',
-                'Mes': 'TOTALES',
+                'Mes': '',
+                'Fecha': 'TOTALES',
                 'Consumo Energia activa (kWh/mes)': x.consumoEnergiaActiva,
                 'Energia activa exportada (kWh/mes)': x.energiaActivaExportada,
                 'Demanda potencia maxima (kW)': x.demandaPotenciaMaxima,
@@ -174,6 +184,25 @@ export class ProveedoresEnergiaComponent implements OnInit {
                 'TOTAL (L)': x.total
               }]
             });
+
+            this.cols = [['Descripcion', ...this.dataExport.map(x => x.Fecha)]]
+            this.dataPDF = [
+              ['PROVEEDOR', ...this.dataExport.map(x => x['PROVEEDOR'])],
+              ['Año', ...this.dataExport.map(x => x['Año'])],
+              ['Consumo Energia activa (kWh/mes)', ...this.dataExport.map(x => x['Consumo Energia activa (kWh/mes)'])],
+              ['Energia activa exportada (kWh/mes)', ...this.dataExport.map(x => x['Energia activa exportada (kWh/mes)'])],
+              ['Demanda potencia maxima (kW)', ...this.dataExport.map(x => x['Demanda potencia maxima (kW)'])],
+              ['Consumo Energia reactiva (kVArh/mes)', ...this.dataExport.map(x => x['Consumo Energia reactiva (kVArh/mes)'])],
+              ['Factor de Potencia', ...this.dataExport.map(x => x['Factor de Potencia'])],
+              ['Costo de la Energia (L/kWh)', ...this.dataExport.map(x => x['Costo de la Energia (L/kWh)'])],
+              ['Costo de la Demanda (L/kW-mes)', ...this.dataExport.map(x => x['Costo de la Demanda (L/kW-mes)'])],
+              ['Costo de la Energia (L)', ...this.dataExport.map(x => x['Costo de la Energia (L)'])],
+              ['Alumbrado Público (L)', ...this.dataExport.map(x => x['Alumbrado Público (L)'])],
+              ['Cargo de Comercialización (L)', ...this.dataExport.map(x => x['Cargo de Comercialización (L)'])],
+              ['Cargo de Regulación (L)', ...this.dataExport.map(x => x['Cargo de Regulación (L)'])],
+              ['Demanda (L)', ...this.dataExport.map(x => x['Demanda (L)'])],
+              ['TOTAL (L)', ...this.dataExport.map(x => x['TOTAL (L)'])]
+            ]
 
             if (this.listOfData.length === 0) {
               this.spinner.hide();
@@ -201,13 +230,68 @@ export class ProveedoresEnergiaComponent implements OnInit {
         );
     }
 
-
-
   }
 
-  exportPdf(){
-    
-  }
+  exportPdf() {
+    let tamano = this.cols[0].length;
 
+    this.colsExport[0] = this.cols[0].slice(0, 4);
+    this.dataPDFExport[0] = this.dataPDF[0].slice(0, 4);
+    this.dataPDFExport[1] = this.dataPDF[1].slice(0, 4);
+    this.dataPDFExport[2] = this.dataPDF[2].slice(0, 4);
+    this.dataPDFExport[3] = this.dataPDF[3].slice(0, 4);
+    this.dataPDFExport[4] = this.dataPDF[4].slice(0, 4);
+    this.dataPDFExport[5] = this.dataPDF[5].slice(0, 4);
+    this.dataPDFExport[6] = this.dataPDF[6].slice(0, 4);
+    this.dataPDFExport[7] = this.dataPDF[7].slice(0, 4);
+    this.dataPDFExport[8] = this.dataPDF[8].slice(0, 4);
+    this.dataPDFExport[9] = this.dataPDF[9].slice(0, 4);
+    this.dataPDFExport[10] = this.dataPDF[10].slice(0, 4);
+    this.dataPDFExport[11] = this.dataPDF[11].slice(0, 4);
+    this.dataPDFExport[12] = this.dataPDF[12].slice(0, 4);
+    this.dataPDFExport[13] = this.dataPDF[13].slice(0, 4);
+    this.dataPDFExport[14] = this.dataPDF[14].slice(0, 4);
+
+    const doc = new jspdf('p', 'in', 'letter');
+    (doc as any).autoTable(
+      {
+        head: this.colsExport,
+        body: this.dataPDFExport,
+        theme: 'striped'
+      }
+    );
+
+    for (let x = 4; x < tamano;) {
+      this.colsExport[0] = this.cols[0].slice(x, (x + 4));
+      this.dataPDFExport[0] = this.dataPDF[0].slice(x, (x + 4));
+      this.dataPDFExport[1] = this.dataPDF[1].slice(x, (x + 4));
+      this.dataPDFExport[2] = this.dataPDF[2].slice(x, (x + 4));
+      this.dataPDFExport[3] = this.dataPDF[3].slice(x, (x + 4));
+      this.dataPDFExport[4] = this.dataPDF[4].slice(x, (x + 4));
+      this.dataPDFExport[5] = this.dataPDF[5].slice(x, (x + 4));
+      this.dataPDFExport[6] = this.dataPDF[6].slice(x, (x + 4));
+      this.dataPDFExport[7] = this.dataPDF[7].slice(x, (x + 4));
+      this.dataPDFExport[8] = this.dataPDF[8].slice(x, (x + 4));
+      this.dataPDFExport[9] = this.dataPDF[9].slice(x, (x + 4));
+      this.dataPDFExport[10] = this.dataPDF[10].slice(x, (x + 4));
+      this.dataPDFExport[11] = this.dataPDF[11].slice(x, (x + 4));
+      this.dataPDFExport[12] = this.dataPDF[12].slice(x, (x + 4));
+      this.dataPDFExport[13] = this.dataPDF[13].slice(x, (x + 4));
+      this.dataPDFExport[14] = this.dataPDF[14].slice(x, (x + 4));
+
+      doc.addPage('p');
+      (doc as any).autoTable(
+        {
+          head: this.colsExport,
+          body: this.dataPDFExport,
+          theme: 'striped'
+        }
+      );
+
+      x = x + 4;
+    }
+
+    doc.save('InformeProveedoresDeEnergia.pdf');
+  }
 
 }
